e39fae69042acae9060970d2026189f01b0d4741
Mohamed Atif <mohamedabotreka40@gmail.com>
2025-03-21 00:03:07 +0200

    complete admin otp verification

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
 import PatientForm from "@/components/forms/PatientForm";
+import PasskeyModal from "@/components/PasskeyModal";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Home() {
+export default async function Home({ searchParams }: SearchParamProps) {
+  const params = await searchParams;
+  const isAdmin = params.admin === "true";
   return (
     <div className="flex h-screen max-h-screen">
+      {isAdmin && <PasskeyModal />}
+
       <section className="remove-scrollbar container my-auto max-h-screen">
         <div className="sub-container max-w-[496px] ">
           <Image
